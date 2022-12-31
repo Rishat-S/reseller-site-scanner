b5b93ee4d9c9b082114997a580bce372d8396b0d
S Rishat <spk_irbis@mail.ru>
2022-12-31 10:09:07 +0300

    Initial commit

---
+++ src/main/java/ru/rishat/App.java
@@ -0,0 +1,13 @@
+package ru.rishat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
