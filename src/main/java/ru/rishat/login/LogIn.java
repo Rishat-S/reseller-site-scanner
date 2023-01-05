00cabe14868fa19ae65ff4f684b1f4edba0b9a27
S Rishat <spk_irbis@mail.ru>
2023-01-05 10:22:52 +0300

    LogIn is edited

--- src/main/java/ru/rishat/login/LogIn.java
+++ src/main/java/ru/rishat/login/LogIn.java
@@ -1,7 +1,7 @@
 package ru.rishat.login;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import ru.rishat.scanner.PositionScanner;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -13,19 +13,20 @@ import static ru.rishat.constants.Constants.XPATH_BUTTON_OK;
 
 public class LogIn {
     private static final Logger logger = Logger.getLogger(LogIn.class.getName());
+    private static final PositionScanner positionScanner = new PositionScanner();
 
     public static void logIn(WebDriver driver) {
         try {
             final BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/auth.csv"));
             final String[] authData = bufferedReader.readLine().split(",");
-            driver.findElement(By.tagName("button")).click();
-            driver.findElement(By.name("fr.email")).click();
-            driver.findElement(By.name("fr.email")).clear();
-            driver.findElement(By.name("fr.email")).sendKeys(authData[0]);
-            driver.findElement(By.name("fr.password")).click();
-            driver.findElement(By.name("fr.password")).clear();
-            driver.findElement(By.name("fr.password")).sendKeys(authData[1]);
-            driver.findElement(By.xpath(XPATH_BUTTON_OK)).click();
+            positionScanner.findElementByTagName(driver, "button").click();
+            positionScanner.findElementByName(driver, "fr.email").click();
+            positionScanner.findElementByName(driver, "fr.email").clear();
+            positionScanner.findElementByName(driver, "fr.email").sendKeys(authData[0]);
+            positionScanner.findElementByName(driver, "fr.password").click();
+            positionScanner.findElementByName(driver, "fr.password").clear();
+            positionScanner.findElementByName(driver, "fr.password").sendKeys(authData[1]);
+            positionScanner.findElementByXpath(driver, XPATH_BUTTON_OK).click();
             logger.log(Level.INFO, "Authorization data is filled");
         } catch (IOException e) {
             logger.log(Level.INFO, "Can't read auth file");
