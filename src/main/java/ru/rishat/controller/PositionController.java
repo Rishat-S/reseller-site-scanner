fed73a7dbb8eebd77ba2d6bcab81e95781a81caf
Rishat Suleymanov <spk_irbis@mail.ru>
2023-01-17 21:31:57 +0300

    Small changes

--- src/main/java/ru/rishat/controller/PositionController.java
+++ src/main/java/ru/rishat/controller/PositionController.java
@@ -3,10 +3,7 @@ package ru.rishat.controller;
 import org.openqa.selenium.WebDriver;
 import ru.rishat.service.PositionServiceImp;
 
-import java.util.logging.Logger;
-
 public class PositionController {
-    private static final Logger logger = Logger.getLogger(PositionController.class.getName());
     PositionServiceImp positionServiceImp = new PositionServiceImp();
 
     public void scanAllPositions(WebDriver driver) throws InterruptedException {
--- src/main/java/ru/rishat/repository/UserRepositoryImpl.java
+++ src/main/java/ru/rishat/repository/UserRepositoryImpl.java
@@ -5,8 +5,10 @@ import ru.rishat.entity.User;
 import static ru.rishat.constants.Constants.DATA_FROM_FILE;
 
 public class UserRepositoryImpl implements UserRepository {
+
     @Override
     public User getUser() {
+        //TODO:
         String[] authData = DATA_FROM_FILE.get(0).split(",");
         return new User(authData[0], authData[1]);
     }
